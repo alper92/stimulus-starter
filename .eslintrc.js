40c1c97506a9ed7ded9a0c19f543c1bfe5ff65b8
Alper Kürtül <149578678+alperkuertuel@users.noreply.github.com>
2024-05-15 15:16:56 +0200

    adds config to linter config
    adds config to linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,12 @@ module.exports = {
   env: {
     browser: true,
   },
-  extends: ['eslint:recommended', 'plugin:import/errors', 'plugin:import/warnings', 'airbnb-base'],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'airbnb-base',
+  ],
   parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2022,
@@ -11,7 +16,7 @@ module.exports = {
   plugins: ['import'],
   rules: {
     'no-unused-vars': 'warn',
-    'class-methods-use-this': 'off',
     'no-console': 'off',
+    'class-methods-use-this': 'off', // needed syntax for stimulus
   },
 };
