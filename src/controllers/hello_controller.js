d8545e17643c6b3f5f1e26e9d515fac98bfb2fa0
Alper Kürtül <149578678+alperkuertuel@users.noreply.github.com>
2024-05-15 12:02:39 +0200

    finishes second exercise

---
+++ src/controllers/hello_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["name"];
+  greet() {
+    console.log(`Hello, ${this.name}!`);
+  }
+  get name() {
+    return this.nameTarget.value;
+  }
+}
+
+// --- Targets Map Important Elements To Controller Properties --- //
+/*
+Stimulus will automatically create a this.nameTarget property which
+returns the first matching target element.
+
+ - if data-hello-target value inside the html-Tag 
+   is called "input" you have to also call
+   the target property with this.inputTarget!
+*/
+
+// Controllers Simplify Refactoring //
+/*
+There is an arsenal of refactoring techniques! We can clean up
+the code using a name getter! And access it with "this.name"!
+You just need to write a class.
+*/
